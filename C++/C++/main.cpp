cb62d0d066ea639932dca4f0b5766d8670a59227
ytemiloluwa <ytemiloluwa@gmail.com>
2023-11-25 00:55:00 +0100

    Undo Comment

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -1,14 +1,14 @@
-//
-//  main.cpp
-//  C++
-//
-//  Created by Temiloluwa on 22/11/2023.
-//
 
-//#include <iostream>
-//
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    std::cout << "Hello, World!\n";
-//    return 0;
-//}
+ main.cpp
+ C++
+
+ Created by Temiloluwa on 22/11/2023.
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
